57ac66de1b917af251b11d4db47e06955f64af52
Neandro Silva <neandrosilva.11@gmail.com>
2022-12-19 12:18:40 -0300

    criando a classe BD e índices dinâmicos

--- Udemy-01/ECMASScript/App1/app.js
+++ Udemy-01/ECMASScript/App1/app.js
@@ -9,6 +9,33 @@ class Despesa {
     }
 }
 
+class Bd {
+
+    constructor() {
+        let id = localStorage.getItem('id')
+
+        if(id === null) {
+            localStorage.setItem('id', 0)
+        }
+    }
+
+    getProximoId() {
+        let proximoId = localStorage.getItem('id') 
+        return parseInt(proximoId) + 1
+    }
+
+    gravar(d) {
+        let id = this.getProximoId()
+
+        localStorage.setItem(id, JSON.stringify(d))
+
+        localStorage.setItem('id', id)
+
+    }
+}
+
+let bd = new Bd()
+
 function cadastrarDespesa() {
     
     let ano = document.getElementById('ano')
@@ -27,9 +54,6 @@ function cadastrarDespesa() {
         valor.value
     )
 
-    gravar(despesa)
+    bd.gravar(despesa)
 }
 
-function gravar(d) {
-    localStorage.setItem('despesa', JSON.stringify(d))
-}
